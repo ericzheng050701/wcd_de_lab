d4f7476a1316c42e31b8ee26896ac0be343d6215
ericzheng050701 <62180522+ericzheng050701@users.noreply.github.com>
2022-10-22 08:55:12 -0400

    Update pyspark.py

--- airflow_lab/pyspark.py
+++ airflow_lab/pyspark.py
@@ -1,4 +1,3 @@
-
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import concat, lit, col
 from pyspark.sql.functions import when
@@ -8,50 +7,47 @@ import argparse
 
 
 def spark_init(spark_name):
-        spark = SparkSession \
-        .builder \
-        .appName(spark_name) \
-        .getOrCreate()
-        return spark
+    spark = SparkSession \
+    .builder \
+    .appName(spark_name) \
+    .getOrCreate()
+    return spark
 
 def read_csv(spark, input_file_url):
-        df=spark.read\
-          .option("delimiter", ",")\
-          .option("header", "true")\
-          .csv(input_file_url)
-        return df
+    df=spark.read\
+    .option("delimiter", ",")\
+    .option("header", "true")\
+    .csv(input_file_url)
+    return df
 
 def write(df, output_file_url):
-        df.write\
-        .option("header", "false")\
-        .mode("overwrite")\
-        .csv(output_file_url)
+    df.write\
+    .option("header", "false")\
+    .mode("overwrite")\
+    .csv(output_file_url)
 
 
 if __name__ == "__main__":
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--spark_name', help="spark_name")
-        parser.add_argument('--input_file_url', help="input file S3 bucket location.")
-        parser.add_argument('--output_file_url', help="output file S3 bucket location.")
-        parser.add_argument('--avg_order_amount', help="avg_order_amount")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--spark_name', help="spark_name")
+    parser.add_argument('--input_file_url', help="input file S3 bucket location.")
+    parser.add_argument('--output_file_url', help="output file S3 bucket location.")
+    parser.add_argument('--avg_order_amount', help="avg_order_amount")
 
-        args = parser.parse_args()
+    args = parser.parse_args()
 
 
-        spark_name = args.spark_name
-        input_file_url = args.input_file_url
-        output_file_url = args.output_file_url
-        avg_order_amount = float(args.avg_order_amount)
+    spark_name = args.spark_name
+    input_file_url = args.input_file_url
+    output_file_url = args.output_file_url
+    avg_order_amount = float(args.avg_order_amount)
 
-        spark=spark_init(spark_name)
-        df=read_csv(spark, input_file_url)
-        df=df.withColumn('big_order', \
-                when(df['order_amount']-avg_order_amount>0, True)
-                .otherwise(False)
-                )
+    spark=spark_init(spark_name)
+    df=read_csv(spark, input_file_url)
+    df=df.withColumn('big_order', when(df['order_amount']-avg_order_amount>0, True).otherwise(False))
 
-        df.show()
-        write(df, output_file_url)
+    df.show()
+    write(df, output_file_url)
 
 
 
