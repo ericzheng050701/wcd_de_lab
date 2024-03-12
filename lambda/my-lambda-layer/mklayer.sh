4f908fef55950f3f3ad0d5abf24e0ccb16efcb8c
Humza Tariq <84482440+humzatariq1994@users.noreply.github.com>
2024-03-11 19:11:50 -0600

    Updated folder name to python3.9

--- lambda/my-lambda-layer/mklayer.sh
+++ lambda/my-lambda-layer/mklayer.sh
@@ -1,5 +1,5 @@
 mkdir my-lambda-layer && cd my-lambda-layer
-mkdir -p aws-layer/python/lib/python3.8/site-packages
-pip3 install -r requirements.txt --target aws-layer/python/lib/python3.8/site-packages
+mkdir -p aws-layer/python/lib/python3.9/site-packages
+pip3 install -r requirements.txt --target aws-layer/python/lib/python3.9/site-packages
 cd aws-layer
 zip -r9 lambda-layer.zip .
